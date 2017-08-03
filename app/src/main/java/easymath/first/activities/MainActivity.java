25ab0ac05bf1c7e0d5413a67c398afdf762f0d4c
Ido Goodis <igoodis@marketo.com>
2017-08-03 10:48:25 +0300

    comment in main activity

--- app/src/main/java/easymath/first/activities/MainActivity.java
+++ app/src/main/java/easymath/first/activities/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		//check
 		// Handle context
 		Firebase.setAndroidContext(this);
 		setContentView(R.layout.activity_main);
